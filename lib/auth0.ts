c1f75c3b7fc0b7de4e6bd36e9f7e2d822eac88a0
vmoskalev@smart-ui.pro <viktormoskalev07@gmail.com>
2025-07-01 21:00:11 +0300

    start

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,8 +1,27 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
+const getAppBaseUrl = () => {
+    if (typeof window !== "undefined") {
+        console.log("is client");
+        return window.location.origin;
+    }
+
+    const rawEnvUrl = process.env.APP_BASE_URL || process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL;
+
+    if (rawEnvUrl && rawEnvUrl !== "undefined") {
+        return rawEnvUrl.startsWith("http") ? rawEnvUrl : `https://${rawEnvUrl}`;
+    }
+
+    if (process.env.VERCEL_ENV === "production" && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+        return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    }
+
+    return "http://localhost:3000";
+};
+
+export const appBaseUrl = getAppBaseUrl();
+console.log({appBaseUrl});
+
 export const auth0 = new Auth0Client({
-  authorizationParameters: {
-    scope: "openid profile email",
-    audience: "urn:https://api.thebuilder.ai",
-  },
-})
+    appBaseUrl: appBaseUrl,
+});
